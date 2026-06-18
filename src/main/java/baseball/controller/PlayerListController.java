c99e891bfaa919486360819b0371d9019dc7f2d9
功祐 高山 <takayama19990316@gmail.com>
2026-06-18 21:34:38 +0900

    add PlayerListController

--- src/main/java/baseball/controller/PlayerListController.java
+++ src/main/java/baseball/controller/PlayerListController.java
@@ -1,7 +1,55 @@
 package baseball.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import baseball.model.PlayerBean;
+import baseball.service.PlayerList;
+
 @WebServlet("/PlayerListController")
 public class PlayerListController extends HttpServlet {
-	
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String jsp = "/player_list.jsp";
+
+		try {
+
+			PlayerList list = new PlayerList();
+
+			ArrayList<PlayerBean> playerList =
+					list.execute();
+
+			request.setAttribute("playerList", playerList);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+			jsp = "/error.jsp";
+
+			request.setAttribute("message",
+					"選手一覧の取得に失敗しました");
+
+		} finally {
+
+			ServletContext context =
+					getServletContext();
+
+			RequestDispatcher rd =
+					context.getRequestDispatcher(jsp);
+
+			rd.forward(request, response);
+
+		}
+	}
 }
